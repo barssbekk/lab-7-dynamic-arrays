01111d37bf139e32184c38f6872fe6d16ab2b7d0
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-15 18:39:15 -0800

    Add names

--- main.cpp
+++ main.cpp
@@ -5,12 +5,24 @@
 using namespace std;
 
 string* reverseArray(string* arr, int size);
+void populateArr(string* arr, int size);
 
 int main() {
     const int SIZE{5};
+
+
     string* myArr{new string[SIZE]};
+    *(myArr + 0) = "Hayes";
+    *(myArr + 1) = "Alex";
+    *(myArr + 2) = "Morgan";
+    *(myArr + 3) = "Nathan";
+    *(myArr + 4) = "Arisu";
+
+    delete[] myArr;
     return 0;
 }
+
+
 
 string* reverseArray(string* arr, int size) {
 
