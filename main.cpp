e7ac2f0f25b725f89e6956ac416e2b123883b74c
Barsbek Azamatov <bars.moderside@gmail.com>
2026-02-15 18:27:53 -0800

    Declare reverseArray()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+string* reverseArray(string* arr, int size);
+
 int main() {
+    const int SIZE{5};
+    string* myArr{new string[SIZE]};
     return 0;
+}
+
+string* reverseArray(string* arr, int size) {
+
 }
